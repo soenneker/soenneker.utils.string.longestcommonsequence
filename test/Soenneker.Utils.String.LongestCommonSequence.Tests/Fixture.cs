ab08fdf74cce024fb1200f8abb19e41cde485cd6
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:35:05 +0000

    xUnit v3

--- test/Soenneker.Utils.String.LongestCommonSequence.Tests/Fixture.cs
+++ test/Soenneker.Utils.String.LongestCommonSequence.Tests/Fixture.cs
@@ -8,7 +8,7 @@ namespace Soenneker.Utils.String.LongestCommonSequence.Tests;
 
 public class Fixture : UnitFixture
 {
-    public override System.Threading.Tasks.Task InitializeAsync()
+    public override System.Threading.Tasks.ValueTask InitializeAsync()
     {
         SetupIoC(Services);
 
